f6765a05efe72b3feb92a7f3fa75db7e8bf1d0e6
Douglas Moreira <33124673+dougmoreira@users.noreply.github.com>
2022-08-06 10:54:01 -0300

    Creating project

---
+++ MovieList/MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by douglas.moreira on 06/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
